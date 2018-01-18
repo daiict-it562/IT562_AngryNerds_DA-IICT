214687b087aa13054b07e9619d24e64c1c741dbd
tasneempulavwala <tasneempulavwala23@gmail.com>
2018-01-18 21:56:01 +0530

    Added Assignment1 files

---
+++ 201711007_Tasneem_Pulavwala/DSL_queries.py
@@ -0,0 +1,52 @@
+import csv
+from pprint import pprint
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+# Returns all documents
+pprint(es.search(index="books", doc_type="book", body={"query":{"match_all":{}}}))
+
+# Search by matching exactly : returns all documents with author containing Tobit
+pprint(es.search(index="books", doc_type="book", body={"query":{"match":\
+    {"author":"Tobit"}}}))
+
+# Search by matching exactly : returns all documents with author containing
+# either Tobit or Davy
+pprint(es.search(index="books", doc_type="book", body={"query":{"match":\
+    {"author":"Tobit Davy"}}}))
+
+# Search by matching phrase : returns documents with title containing
+# the entire phrase
+pprint(es.search(index="books", doc_type="book", body={"query":\
+    {"match_phrase":{"title":"Beach Party"}}}))
+
+# Search-as-you-Type
+pprint(es.search(index="books", doc_type="book", body={"query":\
+    {"match_phrase_prefix":{"title":"Legen"}}}))
+
+#Multi - match query : Matches multiple fields for the query string
+#(Here, the title field is 2 times more important than author field)
+pprint(es.search(index="books", doc_type="book", body={"query":{"multi_match":\
+    {"query":"Brown LLC","fields":["title^2","author"],"type":"phrase"}}}))
+
+# Common terms query: frequently appearing terms are considered stopwords
+pprint(es.search(index="books", doc_type="book", body={"query":{"common":\
+    {"title":{"query":"Softening of the Eg  g","cutoff_frequency":0.0001}}}}))
+
+# Query String query: parsing
+pprint(es.search(index="books", doc_type="book", body={"query":{"query_string":\
+    {"default_field":"title","query":"of AND the"}}}))
+
+# Simple Query String query:
+pprint(es.search(index="books", doc_type="book", body={"query":{\
+    "simple_query_string":{"fields":["title"],"query":"of +-the"}}}))
+
+# Term query : exact matching, usually used for structured data, not full text fields
+pprint(es.search(index="books", doc_type="book", body={"query":{"term":\
+    {"isbn":"037755401-4"}}}))
+
+# Compound Query
+pprint(es.search(index="books", doc_type="book", body={"query": {"bool":\
+    {"must": { "match_all": {} },"filter": { "range": {"price": \
+    {"gte":"10","lte":"20"}}}}}}))
---
+++ 201711007_Tasneem_Pulavwala/mapping.py
@@ -0,0 +1,40 @@
+import csv
+from pprint import pprint
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+if not es.indices.exists(index="books"):
+    pprint(es.indices.create(index="books", body=\
+    {
+        "mappings":{
+            "book":{
+                "properties":{
+                    "title":{"type":"text","index_options":"positions"},
+                    "author":{"type":"text"},
+                    "publisher":{"type":"text"},
+                    "price":{"type":"double"},
+                    "genre":{"type":"text"},
+                    "isbn":{"type":"keyword"},
+                    "publish_date":{"type":"date"}
+                    }
+                }
+            }
+        }))
+
+
+# Read CSV file and create documents
+with open('dummy_data.csv',encoding="utf-8") as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+
+    for row in readCSV:
+        pprint(es.index(index="books", doc_type="book", body=\
+        {
+            "title":row[1],
+            "author":row[2],
+            "publisher":row[3],
+            "price":row[4],
+            "genre":row[5],
+            "isbn":row[6],
+            "publish_date":row[7]
+        }))
